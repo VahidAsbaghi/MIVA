b5c42d80293ee3423f378ac1ae53dbc18bbd383a
vahid asbaghi <vahid.asbaghi@hotmail.com>
2021-09-29 18:51:28 +0330

    Start to impliment Jwt based authentication and authorizations, add code for startup class and create ITokenService, and mixture implementation for microservices with a proposed JWT authentication

---
+++ Backend/MPT/Login/Services/ITokenService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Login.Model;
+
+namespace Login.Services
+{
+    //https://github.com/joydipkanjilal/jwt-aspnetcore/blob/master/jwt-aspnetcore/ITokenService.cs
+    public interface ITokenService
+    {
+        string BuildToken(string key, string issuer, User user);
+        //string GenerateJSONWebToken(string key, string issuer, UserDTO user);
+        bool IsTokenValid(string key, string issuer, string token);
+    }
+}
--- Backend/MPT/Login/Startup.cs
+++ Backend/MPT/Login/Startup.cs
@@ -5,25 +5,68 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace Login
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSwaggerGen(option =>
                 option.SwaggerDoc("Login", new OpenApiInfo {Title = "Login API", Version = "v1"}));
+
+            services.AddAuthentication(auth =>
+            {
+                auth.DefaultAuthenticateScheme =  JwtBearerDefaults.AuthenticationScheme;
+                //auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    ValidAudience = Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                };
+            });
         }
+    
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSession();
+            app.Use(async (context, next) =>
+            {
+                var token = context.Session.GetString("Token");
+                if (!string.IsNullOrEmpty(token))
+                {
+                    context.Request.Headers.Add("Authorization", "Bearer " + token);
+                }
+                await next();
+            });
+            app.UseAuthentication();
             app.UseSwagger();
             app.UseSwaggerUI(setup =>
             {
@@ -35,7 +78,7 @@ namespace Login
             //}
 
             app.UseRouting();
-
+            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
