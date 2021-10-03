424c48095a623dbcd9068a1e16d09bfef086c62d
vahid asbaghi <vahid.asbaghi@hotmail.com>
2021-10-03 20:38:22 +0330

    Added files and folders

---
+++ Backend/MPT/Login/Infrastructure/Helper/AuthenticateService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Login.Controlers;
+using Login.Core.Model;
+using Login.Core.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+
+namespace Login.Application.Services
+{
+    public class AuthenticateService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly ITokenService _tokenService;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticateService(IUserRepository userRepository,ITokenService tokenService,IConfiguration configuration)
+        {
+            _userRepository = userRepository;
+            _tokenService = tokenService;
+            _configuration = configuration;
+        }
+        public async Task<string> Authenticate(Credentials credentials)
+        {
+            var user=await _userRepository.GetByUsername(credentials.Username);
+            if (user==null)
+            {
+                return null;//Todo Generate appropriate exception
+            }
+
+            var passwordHasher = new PasswordHasher<User>();
+            var verificationResult = passwordHasher.VerifyHashedPassword(user, user.PasswordHash, credentials.Password);
+            if (verificationResult==PasswordVerificationResult.Success)
+            {
+                var token=_tokenService.BuildToken(_configuration["Jwt:Key"], _configuration["Jwt:Issuer"], user);
+                return token;
+            }
+            else
+            {
+                return null;//todo throw appropriate exception
+            }
+        }
+    }
+}
---
+++ Backend/MPT/Login/Infrastructure/Helper/MivaAuthorizeAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Login.Infrastructure.Helper
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class MivaAuthorizeAttribute:Attribute,IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = context.HttpContext.User;
+            var token = context.HttpContext.Session.GetString("Authen");
+            if (user == null)
+            {
+                // not logged in
+                context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+            }
+        }
+    }
+}
